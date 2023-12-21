6ca854134fa623929ce90cd1ccb3513d1477f547
MartaShchur <m.matseyko199@gmail.com>
2023-12-21 23:47:12 +0100

    move DB_HOST

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
-
-
-
 import app from "./app.js";
+import { DB_HOST } from "./config.js";
 
 //XypO8KaV7HwZ3AxQ
 
-const DB_HOST="mongodb+srv://Marta:XypO8KaV7HwZ3AxQ@cluster0.29adlx9.mongodb.net/my-contacts?retryWrites=true&w=majority"
+
 mongoose.connect(DB_HOST)
   .then(() => {
   app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+    // console.log("Server running. Use our API on port: 3000")
+    console.log("Database connection successful")
 })
   })
 
