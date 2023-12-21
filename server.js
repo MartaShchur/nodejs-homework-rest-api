00b12509cd806ee6087cc9cf3e07d4af1b7e55ff
MartaShchur <m.matseyko199@gmail.com>
2023-12-21 22:55:47 +0100

    init hw

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+import mongoose from "mongoose";
+
+
+
 import app from "./app.js";
 
-app.listen(3000, () => {
+//XypO8KaV7HwZ3AxQ
+
+const DB_HOST="mongodb+srv://Marta:XypO8KaV7HwZ3AxQ@cluster0.29adlx9.mongodb.net/my-contacts?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+  .then(() => {
+  app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+  })
+
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
