89e97d976aed713043aaeecb09d3cc58512f9cf0
MartaShchur <m.matseyko199@gmail.com>
2024-01-01 14:37:14 +0100

    Update contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -72,7 +72,6 @@ const deleteById = async (req, res, next) => {
 
 const updateStatusContact = async (req, res) => {
   const { id } = req.params;
-  if (!req.body) throw HttpError(400, "missing field favorite");
   const result = await Contact.findByIdAndUpdate(id, req.body);
   if (!result) throw HttpError(404, "Not found");
 
